2a8a3b42d55323cb5bab640c7b8647ddb682329c
Pranvera1234 <pranveratsipa@gmail.com>
2024-06-17 12:28:07 +0100

    Complete Task class

--- src/Task.py
+++ src/Task.py
@@ -1,3 +1,5 @@
+
+# --- Task Class --- #
 class Task:
     """ Class representing a Task entity with attributes: task_id, title, and description. """
     def __init__(self, task_id, title, description):
@@ -7,7 +9,28 @@ class Task:
         self.description = description
 
 
- 
+    # Declare the class variable, with default value, for tasks. 
+    has_been_completed = False
+    is_graded = False
+    grade = 0
+
+    # Method to change 'has_been_completed' task from False to True.
+    def mark_as_complete(self) :
+        self.has_been_completed = True
+
+    # Method to change 'has_been_completed' emails from True to False.
+    def mark_as_incomplete(self) :
+        self.has_been_completed = False
+
+    # Method to change 'is_graded' task from False to True.
+    def set_grade(self, grade) :
+        self.grade = grade
+        self.is_graded = True
+
+    # Method to change 'has_been_completed' and 'is_graded' task from True to False.
+    def submit(self) :
+        self.has_been_completed = False
+        self.is_graded = False
 
 # Sample data
 tasks_data = [

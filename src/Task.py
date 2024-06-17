9853002e78164f9b284d26d82655eb6e8b8037b5
Pranvera1234 <pranveratsipa@gmail.com>
2024-06-17 13:27:53 +0100

    Identation using Black

--- src/Task.py
+++ src/Task.py
@@ -1,47 +1,41 @@
-
 # --- Task Class --- #
 class Task:
-    """ Class representing a Task entity with attributes: task_id, title, and description. """
+    """Class representing a Task entity with attributes."""
+
     def __init__(self, task_id, title, description):
-        """ Initialize a new Task instance. """
+        """Initialize a new Task instance."""
         self.task_id = task_id
         self.title = title
         self.description = description
 
-
-    # Declare the class variable, with default value, for tasks. 
+    # Declare the class variable, with default value, for tasks.
     has_been_completed = False
     is_graded = False
     grade = 0
 
-
     # Method to change 'has_been_completed' task from False to True.
-    def mark_as_complete(self) :
+    def mark_as_complete(self):
         self.has_been_completed = True
 
-
     # Method to change 'has_been_completed' emails from True to False.
-    def mark_as_incomplete(self) :
+    def mark_as_incomplete(self):
         self.has_been_completed = False
 
-
     # Method to change 'is_graded' task from False to True.
-    def set_grade(self, grade) :
+    def set_grade(self, grade):
         self.grade = grade
         self.is_graded = True
 
-
-    # Method to change 'has_been_completed' to True and 'is_graded' task to False.
-    def submit(self) :
+    # Method to change 'has_been_completed' to True and 'is_graded' to False.
+    def submit(self):
         self.has_been_completed = True
         self.is_graded = False
 
-
-    # Method to see if Task has passed 
-    def hasPassed(self) :
-        if self.grade > 80
+    # Method to see if Task has passed.
+    def hasPassed(self):
+        if self.grade > 80:
             return True
-        else
+        else:
             return False
 
 
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,5 +1,3 @@
-
-
 # --- Test Task --- #
 # test_task.py
 import unittest
@@ -9,26 +7,22 @@ from task import Task
 class TestTask(unittest.TestCase):
     def setUp(self):
         # Create a new Task instance before each test
-        self.task = Task("OOP Classes", "Complete a class with unit tests") 
+        self.task = Task("OOP Classes", "Complete a class with unit tests")
 
-    
     def test_mark_as_complete(self):
-        # Test if mark_as_complete method correctly sets mark_as_complete to True
+        # Test if mark_as_complete method sets mark_as_complete to True
         self.task.mark_as_complete()
         self.assertEqual(self.task.mark_as_complete, True)
-    
-    
+
     def test_mark_as_incomplete(self):
-        # Test if mark_as_incomplete method correctly sets mark_as_complete to False
+        # Test if mark_as_incomplete method sets mark_as_complete to False
         self.task.mark_as_incomplete()
         self.assertEqual(self.task.mark_as_incomplete, False)
 
-    
     def test_set_grade_100(self):
         # Test if set_grade method correctly sets grade
         self.task.set_grade(100)
         self.assertEqual(self.task.grade, 100)
-
 
     def test_set_grade_30(self):
         # Test if set_grade method correctly sets grade
@@ -36,11 +30,11 @@ class TestTask(unittest.TestCase):
         self.assertEqual(self.task.grade, 30)
 
     def test_submit(self):
-        # Test if submit method correctly sets has_been_completed to True and is_graded to False
+        # Test if 'submit' sets has_been_completed to True & is_graded to False
         self.task.submit()
         self.assertTrue(self.task.has_been_completed)
         self.assertFalse(self.task.is_graded)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
